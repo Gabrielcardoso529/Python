741e534c71ae7beb2856dc0c34981a2199b8fe3b
Gabrielcardoso529 <bielsantcardoso@gmail.com>
2025-05-14 11:40:38 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,42 @@
+# def exibir_mensagem ():
+#     print('Welcome to functions!!')
+#
+# for n in range(2):
+#     exibir_mensagem()
+#
+# def par_ou_impar():
+#     numero = int(input('Informe um numero: '))
+#     if numero % 2 == 0:
+#         return "este numero é Par"
+#     else:
+#         return 'este numero é Impar'
+#
+# print(par_ou_impar())
+#
+# def calcular_area(base, altura):
+#     area = (base * altura) / 2
+#     print(f'Área do triangulo: {area}')
+#
+# base = float(input("Informe a base: "))
+# altura = float(input("Informe a altura: "))
+#
+# calcular_area(base, altura)
+#
+# raio = float(input('informe um numero: '))
+#
+# def area(r):
+#     area = 3.14 * (r ** 2)
+#     return area
+# print(area(raio))
+#
+# def perimetro(r):
+#     perimetro = 3.14 * 2 * r
+#     return perimetro
+# print(perimetro(raio))
+
+def exibir_mensagem():
+    print('---------------------')
+    print('Bem vindo ao sistema. ')
+    print('---------------------')
+exibir_mensagem()
+
